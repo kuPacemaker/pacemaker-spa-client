573c387e60f62cbbc8d1d497c444fd42e22e1deb
Lumy Kelvin <lunox273@gmail.com>
2020-09-07 18:11:03 +0900

    import font family

--- src/client/root.js
+++ src/client/root.js
@@ -1,16 +1,14 @@
 import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
-import App from 'shared/App';
-import store, { history } from 'store/store';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+
+import App from 'shared/App';
+import store, { history } from 'store/store';
 
 const Root = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      {/* <BrowserRouter> */}
       <App />
-      {/* </BrowserRouter> */}
     </ConnectedRouter>
   </Provider>
 );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import 'resources/font/helvetica/style.css';
+import './resources/font/nanum_gothic/style.css';
+
 import Root from './client/root';
 import { register } from './serviceWorker';
 
