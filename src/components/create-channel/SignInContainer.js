713ae1117a36a52ac050ebc62d943aff2e9db7a1
Lumy Kelvin <lunox273@gmail.com>
2020-09-10 02:20:16 +0900

    make proto directorys

---
+++ src/components/create-channel/SignInContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import SignIn from './view/SignIn';
+
+const SignInContainer = (prop) => {
+  return <SignIn />;
+};
+
+// const mapStateToProps = ({ counter }) => ({
+//   color: counter.color,
+//   number: counter.number,
+// });
+
+// const mapDispatchToProps = (dispatch) =>
+//   bindActionCreators({ incrementAsync, decrement, getPost }, dispatch);
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default SignInContainer;
