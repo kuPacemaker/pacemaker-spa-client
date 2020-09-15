6106fe94652693d8fdf246e1926a84d7b11a01fd
Lumy Kelvin <lunox273@gmail.com>
2020-09-15 22:50:50 +0900

    add newspeed

--- src/components/peed/view/Peed.js
+++ src/components/peed/view/Peed.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import peedImage_1 from 'resources/images/peeds/peed-image-1.jpg';
-import peedImage_2 from 'resources/images/peeds/peed-image-1.jpg';
-import peedImage_3 from 'resources/images/peeds/peed-image-1.jpg';
-import peedImage_4 from 'resources/images/peeds/peed-image-1.jpg';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+import peedImage_1 from 'resources/images/peeds/peed-image-1.png';
+import peedImage_2 from 'resources/images/peeds/peed-image-2.png';
+import peedImage_3 from 'resources/images/peeds/peed-image-3.png';
+import peedImage_4 from 'resources/images/peeds/peed-image-4.png';
+
 import './Peed.scss';
 
 const types = {
-  SYSTEM_NOTICE: ['r', peedImage_1],
-  QUESTION_GENERATION: ['r', peedImage_2],
-  PAPER_RECEIVE: ['l', peedImage_3],
-  PAPER_FINISH: ['l', peedImage_4],
+  SYSTEM_NOTICE: ['SYSTEM', 'RightImagePeed', peedImage_1],
+  QUESTION_GENERATION: ['FOR LEADER', 'RightImagePeed', peedImage_2],
+  PAPER_RECEIVE: ['FOR RUNNER', 'LeftImagePeed', peedImage_3],
+  PAPER_FINISH: ['FOR LEADER', 'LeftImagePeed', peedImage_4],
 };
 
-const Peed = ({ type, title, body }) => {
+const Peed = ({ type, title, body, to, arg }) => {
   return (
-    <div className="Peed">
-      {}
-      <img></img>
+    // <Link to="">
+    <div className={classNames('Peed', types[type][1])}>
+      <img className="PeedImage" src={types[type][2]} alt="" />
+      <div className="MessageBox">
+        <div className="Source">{types[type][0]}</div>
+        <div className="Title">{title}</div>
+        <div className="Body">{body}</div>
+      </div>
     </div>
+    // </Link>
   );
 };
 
