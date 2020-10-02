44903545ddfe2792dc133bee383f68cc6d171ea2
Lumy Kelvin <lunox273@gmail.com>
2020-10-02 23:51:23 +0900

    new version

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,21 @@
 // createStore 와 루트 리듀서 불러오기
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
 import Thunk from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 
 import rootReducer from './modules/reducers';
-// import pender from 'redux-pender';
-
-// import logger from '../lib/loggerMiddleware';
-// 리덕스 개발자도구 적용
-// const devTools =
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const composeEnhancers =
   (typeof window !== 'undefined' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
-// const store = createStore(rootReducer, devTools);
 
 export const history = createBrowserHistory();
 
-const logger = createLogger();
 const store = createStore(
   rootReducer(history),
-  composeEnhancers(applyMiddleware(routerMiddleware(history), Thunk, logger))
+  composeEnhancers(applyMiddleware(routerMiddleware(history), Thunk))
 );
 
 export default store;
