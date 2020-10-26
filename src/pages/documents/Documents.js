ac1bae94c89281a68d1ca9d9516cc4a927c56893
Lumy Kelvin <lunox273@gmail.com>
2020-10-26 21:06:59 +0900

    add document page

---
+++ src/pages/documents/Documents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Document from 'components/about-us';
+
+const articles = [
+  ['Title', ''],
+  ['MainArticle', ''],
+  ['SubArticle', ''],
+  ['SubArticle', ''],
+];
+
+const DocumentPage = () => <Document articles={articles} />;
+
+export default DocumentPage;
--- src/routes/MainPageRouter.js
+++ src/routes/MainPageRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import { Newspeed, Board, Channel, Unit } from 'pages';
+import { Newspeed, Board, Channel, Unit, Documents } from 'pages';
 import { LocalMainPage } from 'common/local-path';
 
 const MainPageRouter = ({ location }) => {
@@ -11,16 +11,11 @@ const MainPageRouter = ({ location }) => {
         <Route exact path={LocalMainPage.board.path} component={Board} />
         <Route exact path={LocalMainPage.channel.path} component={Channel} />
         <Route exact path={LocalMainPage.unit.path} component={Unit} />
-        {/* <Route
-            exact
-            path={LocalMainPage.documents.path}
-            component={Newspeed}
-          />
-          <Route
-            exact
-            path={LocalMainPage.statistics.path}
-            component={Newspeed}
-          /> */}
+        <Route
+          exact
+          path={LocalMainPage.documents.path}
+          component={Documents}
+        />
         <Redirect path={LocalMainPage.root + '*'} to={LocalMainPage.newspeed} />
       </Switch>
     </>
