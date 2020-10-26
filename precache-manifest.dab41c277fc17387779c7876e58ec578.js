384680dcf8eefa10a516abee67900456025731f5
Lumy Kelvin <lunox273@gmail.com>
2020-10-26 17:21:57 +0900

    Updates

---
+++ precache-manifest.dab41c277fc17387779c7876e58ec578.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a180044e0cf801270a2484f3a8d49d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7a19a0a0ce61d9f4dea",
+    "url": "/static/css/main.18396d31.chunk.css"
+  },
+  {
+    "revision": "f1616d049cc66adfee2b",
+    "url": "/static/js/2.9cd2e1dd.chunk.js"
+  },
+  {
+    "revision": "5235a5429e626c15d61916dfe11adc52",
+    "url": "/static/js/2.9cd2e1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a19a0a0ce61d9f4dea",
+    "url": "/static/js/main.333b04ea.chunk.js"
+  },
+  {
+    "revision": "893ed56ee585efdc6377",
+    "url": "/static/js/runtime-main.4fab4020.js"
+  },
+  {
+    "revision": "939d9be61765481422f33937cdae72ca",
+    "url": "/static/media/HelveticaNeueLTProBd.939d9be6.woff"
+  },
+  {
+    "revision": "0718b48072154f9d268e43860e9eed99",
+    "url": "/static/media/HelveticaNeueLTProBdCn.0718b480.woff"
+  },
+  {
+    "revision": "94d37971c4d1807fa553480bec301759",
+    "url": "/static/media/HelveticaNeueLTProBdCnO.94d37971.woff"
+  },
+  {
+    "revision": "2b06abcaeb5ad37330c73cf40730fdcd",
+    "url": "/static/media/HelveticaNeueLTProBdEx.2b06abca.woff"
+  },
+  {
+    "revision": "6e37e08fb5c8ca4b56abea455ca01e89",
+    "url": "/static/media/HelveticaNeueLTProBdExO.6e37e08f.woff"
+  },
+  {
+    "revision": "f31438f99637d9ddb07384db5fb490a0",
+    "url": "/static/media/HelveticaNeueLTProBdIt.f31438f9.woff"
+  },
+  {
+    "revision": "18e359dd46b56d5485a9658598ab9801",
+    "url": "/static/media/HelveticaNeueLTProBdOu.18e359dd.woff"
+  },
+  {
+    "revision": "ec106f14a7a458e809972bc6491568cd",
+    "url": "/static/media/HelveticaNeueLTProBlk.ec106f14.woff"
+  },
+  {
+    "revision": "9f79e2fe6bccad2597ff39c59bab0ef0",
+    "url": "/static/media/HelveticaNeueLTProBlkCn.9f79e2fe.woff"
+  },
+  {
+    "revision": "a1cc82c3b77f459697b492f5ffb47beb",
+    "url": "/static/media/HelveticaNeueLTProBlkCnO.a1cc82c3.woff"
+  },
+  {
+    "revision": "739af2ea0324a7ed11f34986f6504c31",
+    "url": "/static/media/HelveticaNeueLTProBlkEx.739af2ea.woff"
+  },
+  {
+    "revision": "2c14313904c7fbc961fbcc32d58af595",
+    "url": "/static/media/HelveticaNeueLTProBlkExO.2c143139.woff"
+  },
+  {
+    "revision": "a2578f18fb3bbb9db86d283c7ec1a516",
+    "url": "/static/media/HelveticaNeueLTProBlkIt.a2578f18.woff"
+  },
+  {
+    "revision": "f183c4184a9943c332ec1b575cb38328",
+    "url": "/static/media/HelveticaNeueLTProCn.f183c418.woff"
+  },
+  {
+    "revision": "51e9e1718dd7aac8928b4abc3ccc3361",
+    "url": "/static/media/HelveticaNeueLTProCnO.51e9e171.woff"
+  },
+  {
+    "revision": "2b5815e34e255dd82390a5711239a4ef",
+    "url": "/static/media/HelveticaNeueLTProEx.2b5815e3.woff"
+  },
+  {
+    "revision": "a3d533070f90454453f69c05b51b8b67",
+    "url": "/static/media/HelveticaNeueLTProExO.a3d53307.woff"
+  },
+  {
+    "revision": "46f98ccfc40ab4f94c17aa81c2c82ebe",
+    "url": "/static/media/HelveticaNeueLTProHv.46f98ccf.woff"
+  },
+  {
+    "revision": "82b8615b2659e4bedb10d1f5599731e8",
+    "url": "/static/media/HelveticaNeueLTProHvCn.82b8615b.woff"
+  },
+  {
+    "revision": "cd58c328e22ca24096717ef590e8e040",
+    "url": "/static/media/HelveticaNeueLTProHvCnO.cd58c328.woff"
+  },
+  {
+    "revision": "ef08272a630cebf98df48773f0525973",
+    "url": "/static/media/HelveticaNeueLTProHvEx.ef08272a.woff"
+  },
+  {
+    "revision": "390be9ae8be498544048b9a56cb0ff8e",
+    "url": "/static/media/HelveticaNeueLTProHvExO.390be9ae.woff"
+  },
+  {
+    "revision": "d781b537dd095cd1386391eeaa958d5f",
+    "url": "/static/media/HelveticaNeueLTProHvIt.d781b537.woff"
+  },
+  {
+    "revision": "f590313d3cb5d0a3c07fb944f865a0e5",
+    "url": "/static/media/HelveticaNeueLTProIt.f590313d.woff"
+  },
+  {
+    "revision": "db919ecc62d4e3bf6ff877c717cb6b73",
+    "url": "/static/media/HelveticaNeueLTProLt.db919ecc.woff"
+  },
+  {
+    "revision": "7b865dd7e79b2257bf5730ac83b9c512",
+    "url": "/static/media/HelveticaNeueLTProLtCn.7b865dd7.woff"
+  },
+  {
+    "revision": "3331b49376e640c8e4449ab54f85235f",
+    "url": "/static/media/HelveticaNeueLTProLtCnO.3331b493.woff"
+  },
+  {
+    "revision": "d1d2ffbe97f3c2e159dfe72b35993c84",
+    "url": "/static/media/HelveticaNeueLTProLtEx.d1d2ffbe.woff"
+  },
+  {
+    "revision": "4f8d8b805bf75c925d7fa39f591de506",
+    "url": "/static/media/HelveticaNeueLTProLtExO.4f8d8b80.woff"
+  },
+  {
+    "revision": "4357592b91285126d428b0fd24931d44",
+    "url": "/static/media/HelveticaNeueLTProLtIt.4357592b.woff"
+  },
+  {
+    "revision": "3b185f21b4940daa0cbbea1ab88a7f64",
+    "url": "/static/media/HelveticaNeueLTProMd.3b185f21.woff"
+  },
+  {
+    "revision": "c04619e0f63ecb3c83cfb2bc971db981",
+    "url": "/static/media/HelveticaNeueLTProMdCn.c04619e0.woff"
+  },
+  {
+    "revision": "f8439c93d3f5a913ad17e894e5c0b541",
+    "url": "/static/media/HelveticaNeueLTProMdCnO.f8439c93.woff"
+  },
+  {
+    "revision": "c16f4f7b876c24bfbdcebfd8c868b2c0",
+    "url": "/static/media/HelveticaNeueLTProMdEx.c16f4f7b.woff"
+  },
+  {
+    "revision": "5af454267866804c05717fd6dc97b631",
+    "url": "/static/media/HelveticaNeueLTProMdExO.5af45426.woff"
+  },
+  {
+    "revision": "7fbd548ebe788577514212ec4d5e7b33",
+    "url": "/static/media/HelveticaNeueLTProMdIt.7fbd548e.woff"
+  },
+  {
+    "revision": "0194e96487c109dcc138918bbe46faef",
+    "url": "/static/media/HelveticaNeueLTProRoman.0194e964.woff"
+  },
+  {
+    "revision": "3b31f458529db93eb4f191e4eb95458f",
+    "url": "/static/media/HelveticaNeueLTProTh.3b31f458.woff"
+  },
+  {
+    "revision": "123b7600950cdf33dff62ce700fcbde4",
+    "url": "/static/media/HelveticaNeueLTProThCn.123b7600.woff"
+  },
+  {
+    "revision": "bbac73c9626566404ca9473741cf944e",
+    "url": "/static/media/HelveticaNeueLTProThCnO.bbac73c9.woff"
+  },
+  {
+    "revision": "6a52273eef94a5714b1ab361cf6d4e0a",
+    "url": "/static/media/HelveticaNeueLTProThEx.6a52273e.woff"
+  },
+  {
+    "revision": "16713be1aeae4aeca5367df6e4ab31b2",
+    "url": "/static/media/HelveticaNeueLTProThExO.16713be1.woff"
+  },
+  {
+    "revision": "37b4bb6dcb1f56240790cda72983f2c8",
+    "url": "/static/media/HelveticaNeueLTProThIt.37b4bb6d.woff"
+  },
+  {
+    "revision": "ebb6e3d38b28c82cbbf3287abea00bb4",
+    "url": "/static/media/HelveticaNeueLTProUltLt.ebb6e3d3.woff"
+  },
+  {
+    "revision": "0c2b38f3e6058ef3c1621708a0c04005",
+    "url": "/static/media/HelveticaNeueLTProUltLtCn.0c2b38f3.woff"
+  },
+  {
+    "revision": "f6bc0da6561e0f0e08fd93c352b1c040",
+    "url": "/static/media/HelveticaNeueLTProUltLtCnO.f6bc0da6.woff"
+  },
+  {
+    "revision": "da0e44ace26d8e5cd491663cc81eebc6",
+    "url": "/static/media/HelveticaNeueLTProUltLtEx.da0e44ac.woff"
+  },
+  {
+    "revision": "d2e01ae5f7dfbe2971c80cff55962732",
+    "url": "/static/media/HelveticaNeueLTProUltLtExO.d2e01ae5.woff"
+  },
+  {
+    "revision": "eb615e160b46d6eb865cfdf394f0bb62",
+    "url": "/static/media/HelveticaNeueLTProUltLtIt.eb615e16.woff"
+  },
+  {
+    "revision": "862cf6e3d13ebd38095bd76ac431f054",
+    "url": "/static/media/aboutus-image.862cf6e3.jpg"
+  },
+  {
+    "revision": "d67f626b1dd75d5865b1213bd71a539e",
+    "url": "/static/media/account-image.d67f626b.jpg"
+  },
+  {
+    "revision": "b64b6dc000e4654bed460e6dc07d5ffa",
+    "url": "/static/media/background-right.b64b6dc0.png"
+  },
+  {
+    "revision": "f52ccc43746e2f202bc57e9ac2257378",
+    "url": "/static/media/background-up.f52ccc43.png"
+  },
+  {
+    "revision": "2c97bbb465fa16b5606e1f25ea25484e",
+    "url": "/static/media/channel-image-0.2c97bbb4.jpg"
+  },
+  {
+    "revision": "a89f4c7c3ab6dc016c3feba723785590",
+    "url": "/static/media/channel-image-1.a89f4c7c.jpg"
+  },
+  {
+    "revision": "522dbfa796b4d9cea4bad4cb6ace9bee",
+    "url": "/static/media/channel-image-2.522dbfa7.jpg"
+  },
+  {
+    "revision": "5ee23d14c6a7f0a5a3e1d2c8bb51d297",
+    "url": "/static/media/channel-image-3.5ee23d14.jpg"
+  },
+  {
+    "revision": "6c903e733d8f7126222f740e8b9fa2a6",
+    "url": "/static/media/channel-image-4.6c903e73.jpg"
+  },
+  {
+    "revision": "4d8bec1ea12093ac2bb85acf1e727862",
+    "url": "/static/media/channel-image-5.4d8bec1e.jpg"
+  },
+  {
+    "revision": "104c69a5f2c506f096b2046fc8620d73",
+    "url": "/static/media/channel-image-6.104c69a5.jpg"
+  },
+  {
+    "revision": "998b089a749d02b487a6554c3f7abe00",
+    "url": "/static/media/channel-image-7.998b089a.jpg"
+  },
+  {
+    "revision": "770d4f852d2000cf64bd5b04c71f355e",
+    "url": "/static/media/find-account-image.770d4f85.jpg"
+  },
+  {
+    "revision": "c7d6d2ac4229410749d47cc36af9df2b",
+    "url": "/static/media/home-image.c7d6d2ac.jpg"
+  },
+  {
+    "revision": "2b0effb8bfe3757b1951d2a92fda9963",
+    "url": "/static/media/peed-image-1.2b0effb8.png"
+  },
+  {
+    "revision": "17147648bc19afe161a30887526d05c4",
+    "url": "/static/media/peed-image-2.17147648.png"
+  },
+  {
+    "revision": "bcfcb205fdd2609bd922f69d16d4dc9a",
+    "url": "/static/media/peed-image-3.bcfcb205.png"
+  },
+  {
+    "revision": "18adc76cceab321c1ea59c260093d3d3",
+    "url": "/static/media/peed-image-4.18adc76c.png"
+  },
+  {
+    "revision": "236976872da9d07ab9437201d7b4bfd7",
+    "url": "/static/media/signin-image.23697687.jpg"
+  },
+  {
+    "revision": "84e1d2dd5c675246c279062203fe43d5",
+    "url": "/static/media/signup-image.84e1d2dd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pacemaker-spa-client/precache-manifest.a9e9b73f2a73dcd196b21e599ade7975.js"
+  "/precache-manifest.dab41c277fc17387779c7876e58ec578.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pacemaker-spa-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
