8874abd54a0a0ef4923de1f3699e729513d6ccda
Lumy Kelvin <lunox273@gmail.com>
2020-10-30 03:35:28 +0900

    remove sample

--- src/store/modules/.sample/creators/waiting.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-import { CHANGE_INPUT, CREATE, ENTER, LEAVE } from '../type/waiting';
-// createAction 으로 액션 만들기
-let id = 3;
-export const changeInput = createAction(CHANGE_INPUT, (text) => text);
-export const create = createAction(CREATE, (text) => ({ text, id: id++ }));
-export const enter = createAction(ENTER, (id) => id);
-export const leave = createAction(LEAVE, (id) => id);
