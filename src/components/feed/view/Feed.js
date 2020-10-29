65eaeeccfd61eb7c7aa1fb1e864fdc9922fdb627
Lumy Kelvin <lunox273@gmail.com>
2020-10-30 03:31:28 +0900

    set random feed

--- src/components/feed/view/Feed.js
+++ src/components/feed/view/Feed.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames';
 
 import { LocalMainPage } from 'common/local-path';
 import feedImage_1 from 'resources/images/feeds/feed-image-1.png';
@@ -11,10 +10,9 @@ import feedImage_4 from 'resources/images/feeds/feed-image-4.png';
 import './Feed.scss';
 
 const types = {
-  SYSTEM_NOTICE: ['SYSTEM', 'RightImageFeed', feedImage_1, '', ''],
+  SYSTEM_NOTICE: ['SYSTEM', feedImage_1, '', ''],
   QUESTION_GENERATION: [
     'FOR LEADER',
-    'RightImageFeed',
     feedImage_2,
     {
       to: 'unit',
@@ -24,7 +22,6 @@ const types = {
   ],
   PAPER_RECEIVE: [
     'FOR RUNNER',
-    'LeftImageFeed',
     feedImage_3,
     {
       to: 'unit',
@@ -34,7 +31,6 @@ const types = {
   ],
   PAPER_FINISH: [
     'FOR LEADER',
-    'LeftImageFeed',
     feedImage_4,
     {
       to: 'channel',
@@ -43,22 +39,24 @@ const types = {
     },
   ],
 };
-/**
- *
- * channl : `${LocalMainPage.channel.root}${type}?id=${channelId}`
- * unit :   `${LocalMainPage.unit.root}${type}?channel=${channel}&id=${id}&tab=root`;
- */
+
+const makeRandomLR = () => parseInt(Math.random() + 0.5) === 0;
+
 const Feed = ({ type, title, body, arg }) => {
   const to =
-    types[type][3].to === 'unit'
-      ? `${LocalMainPage.unit.root}${types[type][3].type}?channel=${arg.channel_id}&id=${arg.unit_id}&tab=${types[type][3].tab}`
-      : `${LocalMainPage.channel.root}${types[type][3].type}?id=${arg.channel_id}`;
+    types[type][2].to === 'unit'
+      ? `${LocalMainPage.unit.root}${types[type][2].type}?channel=${arg.channel_id}&id=${arg.unit_id}&tab=${types[type][2].tab}`
+      : `${LocalMainPage.channel.root}${types[type][2].type}?id=${arg.channel_id}`;
+  const [isRight, setPosition] = useState(null);
+  useEffect(() => {
+    setPosition(makeRandomLR());
+  }, []);
   return (
-    <div className={classNames('Feed', types[type][1])}>
+    <div className={'Feed ' + (isRight ? 'RightImageFeed' : 'LeftImageFeed')}>
       <img
         draggable="false"
         className="FeedImage"
-        src={types[type][2]}
+        src={types[type][1]}
         alt=""
       />
       <div className="MessageBox">
