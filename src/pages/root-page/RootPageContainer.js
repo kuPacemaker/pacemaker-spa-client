2e553c524cdca30f810a4c8ae1cd59a3f04b2089
Lumy Kelvin <lunox273@gmail.com>
2020-11-01 01:17:34 +0900

    test preload

--- src/pages/root-page/RootPageContainer.js
+++ src/pages/root-page/RootPageContainer.js
@@ -8,21 +8,13 @@ import { refresh } from 'store/modules/action/refresh';
 import LocalPath from 'common/local-path';
 import { decode } from 'common/security/common';
 import RootPage from './RootPage';
+import { preload } from 'common/utility/preload';
 
-// import Images from 'resources/images';
-// const preload = (images) => {
-//   for (const key in images) {
-//     if (images.hasOwnProperty(key)) {
-//       const element = images[key];
-//       let image = new Image();
-//       image.src = element;
-//     }
-//   }
-// };
+import Images from 'resources/images';
 
 const RootPageContainer = (props) => {
   useEffect(() => {
-    // preload(Images);
+    preload(Images);
     if (localStorage.hasOwnProperty('account')) {
       try {
         props.requestSignIn(
