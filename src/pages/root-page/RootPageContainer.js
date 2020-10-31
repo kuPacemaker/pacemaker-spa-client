db30cdd494f07b4e5a05fc7153e88ba9b6e0a3e5
Lumy Kelvin <lunox273@gmail.com>
2020-11-01 02:36:52 +0900

    Revert "Revert "Merge branch 'release' into 9S-Agent""
    This reverts commit ca7df6658b35963beba0f2de1f6ddd4bf1beaed7.

--- src/pages/root-page/RootPageContainer.js
+++ src/pages/root-page/RootPageContainer.js
@@ -4,12 +4,18 @@ import { bindActionCreators } from 'redux';
 import { useHistory } from 'react-router-dom';
 
 import { requestSignIn } from 'store/modules/action/account';
+import { show } from 'store/modules/creators/modal';
 import { refresh } from 'store/modules/action/refresh';
 import LocalPath from 'common/local-path';
 import { decode } from 'common/security/common';
+import { preload } from 'common/utility/preload';
+
 import RootPage from './RootPage';
 
+import Images from 'resources/images';
+
 const RootPageContainer = (props) => {
+  preload(Images);
   useEffect(() => {
     if (localStorage.hasOwnProperty('account')) {
       try {
@@ -24,6 +30,7 @@ const RootPageContainer = (props) => {
       }
     }
   }, []);
+
   const history = useHistory();
   const [intervalId, setIntervalId] = useState(0);
   useEffect(() => {
@@ -35,7 +42,10 @@ const RootPageContainer = (props) => {
     }
     if (intervalId === 0) {
       const id = setInterval(() => {
-        props.refresh({ token: props.token });
+        props.refresh({ token: props.token }, (success, message) => {
+          if (success);
+          else props.show('ERROR MODAL', { message });
+        });
       }, 30000);
       setIntervalId(id);
     }
@@ -49,6 +59,6 @@ const mapStateToProps = ({ account }) => ({
 });
 
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ requestSignIn, refresh }, dispatch);
+  bindActionCreators({ requestSignIn, refresh, show }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(RootPageContainer);
